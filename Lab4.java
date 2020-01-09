dffba272ee1ce78e3b91c7bc17d93c85ecfdd190
reb-github <55207705+reb-github@users.noreply.github.com>
2020-01-09 14:26:25 -0500

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -23,11 +23,30 @@ public class Lab4
   public static void completeBars()
   {
     //insert instructions below
-
-    
-    
+    while (Robot.frontIsClear())
+    {
+      oneBar();
+      Robot.move();
+    }
+    oneBar();
   }
   
+  public static void oneBar()
+  {
+    Robot.turnLeft();
+    while (!Robot.onDark())
+    {
+     Robot.makeDark();
+     Robot.move();
+    }
+    turnAround();
+    while (Robot.frontIsClear())
+    {
+     Robot.move();   
+    }
+    Robot.turnLeft();
+    }
+    
   public static void testCompleteBars1()
   {
     Robot.load("bars1.txt");
@@ -45,11 +64,45 @@ public class Lab4
   public static void combinePiles()
   {
     //insert instructions below
-
-    
+    while (Robot.frontIsClear())
+    {
+     oneBlock();   
+    }
+    oneBlock();
     
   }
 
+  public static void oneBlock()
+  {
+    if (Robot.onDark())
+    {
+     Robot.makeLight();
+     turnRight();
+     Robot.move();
+     Robot.turnLeft();
+     while (Robot.onDark())
+     {
+       Robot.move();  
+        }
+     Robot.makeDark();
+     while (Robot.frontIsClear())
+     {
+       Robot.move();
+        }
+     Robot.turnLeft();
+     Robot.move();
+     Robot.turnLeft();
+    }
+    while (Robot.frontIsClear())
+     {
+       Robot.move();
+      }
+    turnAround();
+    while (!Robot.onDark()&&Robot.frontIsClear())
+    {
+      Robot.move();  
+    }
+    }
   public static void testCombinePiles1()
   {
     Robot.load("piles1.txt");
@@ -67,11 +120,100 @@ public class Lab4
   public static void connectDots()
   {
     //insert instructions below
-
-    
-    
+    while (Robot.onDark())
+    {
+     doADot();
+    }
   }
   
+  
+  public static void doADot()
+  {
+     if (goLeft())
+     {
+       Robot.turnLeft();
+       Robot.move();
+       Robot.makeDark();
+       Robot.move();
+        }
+     else if (goRight())
+     {
+       turnRight(); 
+       Robot.move();
+       Robot.makeDark();
+       Robot.move();
+        }
+     else if (goStraight())
+     {
+       Robot.move();
+       Robot.makeDark();
+       Robot.move();  
+        }
+     else
+     {
+       Robot.move();  
+        }
+    }
+  
+  
+    public static boolean goLeft()
+  {
+    Robot.turnLeft();
+    Robot.move();
+    Robot.move();
+    if (Robot.onDark())
+    {
+      turnAround();
+      Robot.move();
+      Robot.move();
+      Robot.turnLeft();
+      return true;  
+    }
+    turnAround();
+    Robot.move();
+    Robot.move();
+    Robot.turnLeft();
+    return false;
+  }
+    
+  public static boolean goRight()
+  {
+    turnRight();
+    Robot.move();
+    Robot.move();
+    if (Robot.onDark())
+    {
+      turnAround();
+      Robot.move();
+      Robot.move();
+      turnRight();
+      return true;  
+    }
+    turnAround();
+    Robot.move();
+    Robot.move();
+    turnRight();
+    return false;
+  }
+  
+  public static boolean goStraight()
+  {
+    Robot.move();
+    Robot.move();
+    if (Robot.onDark())
+    {
+      turnAround();
+      Robot.move();
+      Robot.move();
+      turnAround();
+      return true;  
+    }
+    turnAround();
+    Robot.move();
+    Robot.move();
+    turnAround();
+    return false;
+  }
   public static void testConnectDots1()
   {
     Robot.load("connect1.txt");
